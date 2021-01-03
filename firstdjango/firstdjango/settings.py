6183a2754ef1a6dc9c7a8287c01c9683c5afc70b
amoghkl <38749514+amoghkl@users.noreply.github.com>
2021-01-03 17:01:20 +0530

    Forth Commit

--- firstdjango/firstdjango/settings.py
+++ firstdjango/firstdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd@be-l%uf77czpe@@-06h4h*wewnuq(%++eg_!5kndlyfx&61('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','djangotestamogh.herokuapp.com']
 
 
 # Application definition
