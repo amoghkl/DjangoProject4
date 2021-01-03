1fc47d40637c58d0b05f845421d9394febf57cce
amoghkl <38749514+amoghkl@users.noreply.github.com>
2021-01-03 21:57:57 +0530

    Update settings.py

--- firstdjango/firstdjango/settings.py
+++ firstdjango/firstdjango/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "/firstdjango/static")]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
